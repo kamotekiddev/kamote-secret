70c0cc014e4ed1a1ab48e55837598a9f9013c3aa
kamote-kid <kamote-kid@gmail.com>
2023-07-30 14:08:55 +0800

    create rename vault api

--- app/api/vaults/[vaultId]/route.ts
+++ app/api/vaults/[vaultId]/route.ts
@@ -17,11 +17,39 @@ export async function DELETE(req: Request, { params }: Params) {
     if (!id)
       return NextResponse.json({ message: "Invalid Data" }, { status: 400 });
 
-    const deleteVault = await prismadb.vault.delete({ where: { id } });
+    const deletedVault = await prismadb.vault.delete({ where: { id } });
 
     return NextResponse.json({
       message: "Vault deleted successfully",
-      vault: deleteVault,
+      vault: deletedVault,
+    });
+  } catch (error) {
+    return NextResponse.json(error, { status: 500 });
+  }
+}
+
+export async function PUT(req: Request, { params }: Params) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const id = params.vaultId;
+    const { name } = body;
+
+    if (!userId)
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    if (!id)
+      return NextResponse.json({ message: "Invalid Data" }, { status: 400 });
+
+    const updatedVault = await prismadb.vault.update({
+      where: { id },
+      data: {
+        name,
+      },
+    });
+
+    return NextResponse.json({
+      message: "Vault updated successfully",
+      vault: updatedVault,
     });
   } catch (error) {
     return NextResponse.json(error, { status: 500 });
