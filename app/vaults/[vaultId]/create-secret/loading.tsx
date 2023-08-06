b4ab9bad5206378b6e2bc046f9eccb7c24903616
kamote-kid <kamote-kid@gmail.com>
2023-08-06 15:53:57 +0800

    create a loading spinner per page load

---
+++ app/vaults/[vaultId]/create-secret/loading.tsx
@@ -0,0 +1,11 @@
+import { Center, Spinner } from "@/components/chakra-components";
+
+const Loading = () => {
+  return (
+    <Center>
+      <Spinner />
+    </Center>
+  );
+};
+
+export default Loading;
