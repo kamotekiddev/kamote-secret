e40177b6592e4342d3bbf813e8c7067757d6481b
kamote-kid <kamote-kid@gmail.com>
2023-08-06 12:49:47 +0800

    bugfixed: not protecting the route

--- middleware.ts
+++ middleware.ts
@@ -3,10 +3,11 @@ import { withAuth } from "next-auth/middleware";
 export default withAuth({
   pages: {
     signIn: "/sign-in",
+    signOut: "/sign-in",
     error: "/auth-error",
   },
 });
 
 export const config = {
-  matcher: ["/vaults:path*"],
+  matcher: ["/vaults/:path*"],
 };
