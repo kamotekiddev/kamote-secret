fe89dc414f40bfb1d61581bd6651f93e694fee21
kamote-kid <kamote-kid@gmail.com>
2023-09-27 10:41:42 +0800

    redirect to vaults user is already signed in

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,7 +1,12 @@
+import { redirect } from "next/navigation";
 import { Center } from "@/components/chakra-components";
 import SignInForm from "./SignInForm";
+import getCurrentUser from "@/libs/getCurrentUser";
 
-export default function SimpleCard() {
+export default async function SignInPage() {
+  const user = await getCurrentUser();
+  if (user) redirect("/vaults");
+
   return (
     <Center h="100vh">
       <SignInForm />
